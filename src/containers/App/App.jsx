a81d8acb41be7914c38379aa3ebaffdfb2c35c35
XUSdLfXD <devilwork123123@gmail.com>
2023-02-06 15:16:15 +0200

    project env

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,11 @@
+import classes from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={classes.App}>
+      <h1 style={{ textAlign: "center" }}>Test</h1>
+    </div>
+  );
+}
+
+export default App;
